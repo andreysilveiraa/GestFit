e8cf3cdd173740b7ccadd1570ba8d1c0a59de046
ALUNO J16 <alunoj16@unifor.local>
2026-04-22 20:03:44 -0300

    Substituindo frontend por estrutura Spring Boot

---
+++ demo/src/main/java/com/gestfit/GestfitApplication.java
@@ -0,0 +1,13 @@
+package com.gestfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestfitApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/gestfit/GestfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
